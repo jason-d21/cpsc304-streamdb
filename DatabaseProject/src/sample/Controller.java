e28f5306d77e42ae8ceb9eaebbccc62a9ac3a67d
Moses Hsu <moses@zaber.com>
2022-03-26 20:26:49 -0700

    add basic UI design

--- DatabaseProject/src/sample/Controller.java
+++ DatabaseProject/src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private ComboBox<String> templates;
+
+    @FXML
+    void getTemplates(MouseEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        templates.getItems().addAll("Insert", "Delete", "Update", "Select");
+    }
 }
