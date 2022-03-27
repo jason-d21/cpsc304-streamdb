9ef21399fd9414e5a584318876225393994afbe0
Moses Hsu <moses@zaber.com>
2022-03-27 13:56:46 -0700

    minor UI fixes

--- DatabaseProject/src/MovieDatabase/Controller.java
+++ DatabaseProject/src/MovieDatabase/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,10 +16,22 @@ public class Controller implements Initializable {
     private ComboBox<String> templates;
 
     @FXML
+    private ComboBox<String> defaultQueries;
+
+    @FXML
     private TextArea terminal;
 
     @FXML
-    void Select(ActionEvent event) {
+    private TextArea output;
+
+    @FXML
+    void RunCommand(MouseEvent event) {
+        String command = terminal.getText().replace("\n", "").replace("\r", "");
+        output.setText("PLACEHOLDER OUTPUT");
+    }
+
+    @FXML
+    void SelectTemplate(ActionEvent event) {
         String option = templates.getSelectionModel().getSelectedItem();
         if (option.equals("Insert")) {
             terminal.setText("INSERT INTO table_name\n" +
@@ -31,12 +44,26 @@ public class Controller implements Initializable {
                     "WHERE condition;");
         } else if (option.equals("Select")) {
             terminal.setText("SELECT column1, column2, ...\n" +
-                    "FROM table_name;");
+                    "FROM table_name\n" +
+                    "WHERE condition;");
+        }
+    }
+
+    @FXML
+    void SelectQuery(ActionEvent event) {
+        String option = defaultQueries.getSelectionModel().getSelectedItem();
+        if (option.equals("Query1")) {
+            terminal.setText("Query1");
+        } else if (option.equals("Query2")) {
+            terminal.setText("Query2");
+        } else if (option.equals("Query3")) {
+            terminal.setText("Query3");
         }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         templates.getItems().addAll("Insert", "Delete", "Update", "Select");
+        defaultQueries.getItems().addAll("Query1", "Query2", "Query3");
     }
 }
